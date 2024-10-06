075755f27132ac14161720571108eaba86f0a7cb
Conar365 <kvazar382@gmail.com>
2024-10-06 22:25:20 +0300

    aap

--- compilete_dev.py
+++ compilete_dev.py
@@ -7,3 +7,5 @@ if int(let) == 0:
     name = input("Name: ")
     os.system(f'git commit -m "{name}"')
     os.system("git push")
+elif int(let) == 1:
+    os.system("sass prefab/main.scss static/css/main.css")
