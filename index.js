6032bb56d390abba78448fb5ee8c074ce5ae79c4
Conar365 <kvazar382@gmail.com>
2024-11-17 14:31:48 +0300

    file save

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const multer = require('multer');
 
 const app = express();
 const PORT = 3000;
+
+let time_del = 0;
+
+const generateUniqueFileName = (originalName, destination) => {
+    let fileName = originalName;
+    let filePath = path.join(destination, fileName);
+    let counter = 1;
+
+    while (fs.existsSync(filePath)) {
+        const extname = path.extname(fileName);
+        const basename = path.basename(fileName, extname);
+        fileName = `${basename}_${counter}${extname}`;
+        filePath = path.join(destination, fileName);
+        counter++;
+    }
+
+    return fileName;
+};
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, 'static', 'file'));
+    },
+    filename: (req, file, cb) => {
+        const uniqueFileName = generateUniqueFileName(file.originalname, path.join(__dirname, 'static', 'file'));
+        cb(null, uniqueFileName);
+    }
+});
+
+const upload = multer({ storage: storage, limits: { fileSize: 10 * 1024 * 1024 } }).single('file');
 
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
@@ -11,15 +42,59 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    const fileDir = path.join(__dirname, 'static', 'file');
-    fs.readdir(fileDir, (err, files) => {
+    res.render('index');
+});
+
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
         if (err) {
-            return res.status(500).send('Не вдалося отримати файли');
+            return res.status(500).json({ success: false, message: 'Не вдалося завантажити файл', error: err.message });
         }
 
-        res.render('index', { files });
+        res.json({ success: true, message: 'Файл успішно завантажено', fileName: req.file.filename });
     });
 });
+
+const clearFileFolder = (folderPath) => {
+    fs.readdir(folderPath, (err, files) => {
+        if (err) {
+            console.error("Не вдалося прочитати вміст папки:", err);
+            return;
+        }
+
+        files.forEach(file => {
+            const filePath = path.join(folderPath, file);
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    console.error("Не вдалося видалити файл:", err);
+                } else {
+                    console.log(`Файл ${file} видалено.`);
+                }
+            });
+        });
+    });
+};
+
+app.post('/time', (req, res) => {
+    res.json({ time: time_del });
+});
+
+setInterval(() => {
+    const fileDir = path.join(__dirname, 'static', 'file');
+    clearFileFolder(fileDir);
+}, 1800000);
+
+function func_time_del() {
+    setInterval(() => {
+        if (time_del === 1800) {
+            time_del = 0;
+        }
+
+        time_del += 1;
+    }, 1000);
+}
+
+func_time_del();
 
 app.listen(PORT, () => {
     console.log(`Сервер запущено на http://localhost:${PORT}`);
--- static/main.js
+++ static/main.js
@@ -3,3 +3,66 @@ $("#file_input_qfEASD").on("change", function () {
 
     $("#file_name").text(fileName);
 });
+
+function get_file_render() {
+    $(".fwewsdwq32r").html(null);
+
+    $.ajax({
+        url: '/file',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(null),
+        success: function (response) {
+            let data = response['files'];
+
+            if (data.length === 0) {
+                let text = `<p class="qe289123d">null</p> <br>`;
+
+                $(".fwewsdwq32r").append(text);
+            } else {
+                for (let i = 0; i < data.length; i++) {
+                    let text = `<a class="qe289123d" download href="static/file/${data[i]}">${data[i]}</a> <br>`;
+    
+                    $(".fwewsdwq32r").append(text);
+                }
+            }
+        },
+        error: function (xhr, status, error) {
+            console.log("Error: " + error);
+            console.log("Response text:", xhr.responseText);
+        }
+    });
+}
+
+
+function file_send() {
+    var fileInput = $('#file_input_qfEASD')[0];
+
+    var file = fileInput.files[0];
+    if (!file) {
+        alert('Будь ласка, виберіть файл!');
+        return;
+    }
+
+    var formData = new FormData();
+    formData.append('file', file);
+
+    $.ajax({
+        url: '/upload',
+        type: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function(data) {
+            if (data.success) {
+                alert('Файл успішно завантажено!');
+            } else {
+                alert('Сталася помилка при завантаженні файлу!');
+            }
+        },
+        error: function(error) {
+            console.error('Помилка:', error);
+            alert('Сталася помилка при завантаженні файлу!');
+        }
+    });
+}
